c5ad577ba78cdd251775f8e000671190d8328d4c
Wesley Soo-Hoo <wesoohoo@gmail.com>
2021-09-16 10:39:44 -0400

    add test_plot

---
+++ test/test_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+zdata = 15 * np.random.random(100)
+xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+ax.scatter3D(xdata, ydata, zdata, marker='.');
+plt.show()
+
