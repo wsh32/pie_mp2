c73c3f6041246b3fa1f6dcc50b6bb3e838ea1e0c
Wesley Soo-Hoo <wesoohoo@gmail.com>
2021-09-25 19:53:14 -0400

    simplify ir calibrations

--- ir_calibration/curve.m
+++ ir_calibration/curve.m
@@ -1,8 +1,6 @@
 dist = [5:5:60]';
 read = [560, 435, 316, 235, 189, 154, 132, 116, 100, 93, 80, 72]';
 
-plot(dist, read, '-rx')
-hold on
 
 f = fit(dist, read,'exp1')
 
@@ -19,10 +17,18 @@ d = -203.1713;
 
 
 % plot(read, a*exp(b*dist), 'blue')
-plot(x, f1, 'green')
+
 % plot(x, c+d*log(x), 'black')
-hold off
 
 syms x
 f = p(1,1) * x^3 + p(1,2) * x^2 + p(1,3) * x + p(1,4);
-g = finverse(f)
+yay = finverse(f)
+g = ((((96525*x)/1088 - 9008)^2 + 654710)^(1/2) - (96525*x)/1088 + 9008)^(1/3) - 16287/(187*((((96525*x)/1088 - 9008)^2 + 654710)^(1/2) - (96525*x)/1088 + 9008)^(1/3)) + 20779/448
+
+boo = simplify(yay)
+
+dummy = 5:0.05:600;
+plot(dummy, subs(g, x, dummy), '-b')
+hold on
+plot(read, dist, '-r')
+hold off
